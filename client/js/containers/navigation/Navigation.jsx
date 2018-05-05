58fc251ed17c362f1e0058c30283e279dfa8383e
Johannes Katajamäki <jkatajamki@gmail.com>
2018-05-05 18:30:28 +0300

    collapsed navbar

--- client/js/containers/navigation/Navigation.jsx
+++ client/js/containers/navigation/Navigation.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import getMenuItems from '../../items/getMenuItems';
 import bindState from '../../utilities/bindState';
 import { withRouter } from 'react-router-dom';
-import { Button } from 'reactstrap';
+import { Button, Collapse } from 'reactstrap';
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
-import { faCaretSquareDown, faCaretSquareUp } from '@fortawesome/fontawesome-free-solid/';
+import { faAngleDown, faAngleUp } from '@fortawesome/fontawesome-free-solid/';
+import classNames from 'classnames/bind';
 
 const getResetNavState = () => ({
   expanded: false,
@@ -21,8 +22,15 @@ class Navigation extends React.Component {
     });
   };
 
+  getCollapseNavClasses = (menuItemTitle, routeTitle) => classNames(
+    'nav-link',
+    { 'active': menuItemTitle === routeTitle, }
+  );
+
   render() {
-    const { props, handleNavToggle, state } = this;
+    const {
+      props, handleNavToggle, state, getCollapseNavClasses
+    } = this;
     const menuItems = getMenuItems();
     const routeTitle = menuItems.find(item => item.url === props.location.pathname).title || '';
 
@@ -33,25 +41,21 @@ class Navigation extends React.Component {
             <a href="#" className="navbar-brand d-flex align-items-center">
               <span className="navbar-brand-text">{routeTitle}</span>
             </a>
-            <Button className="navbar-toggler" data-toggle="collapse"
-                    data-target="#navbarHeader" aria-controls="navbarHeader"
-                    aria-expanded="false" aria-label="Toggle navigation"
-                    onClick={handleNavToggle}
-            >
-              <FontAwesomeIcon className="nav-toggle-icon" icon={state.expanded ? faCaretSquareUp : faCaretSquareDown}/>
+            <Button className="navbar-toggler navbar-toggle" data-toggle="collapse" onClick={handleNavToggle}>
+              <FontAwesomeIcon className="nav-toggle-icon" icon={state.expanded ? faAngleUp : faAngleDown}/>
             </Button>
 
-            <div id="navbar" className="collapse navbar-collapse">
+            <Collapse isOpen={state.expanded} className="navbar-collapse">
               <ul className="nav navbar-nav">
                 {menuItems.map(menuItem =>
-                  <li key={menuItem.id}>
+                  <li key={menuItem.id} className={getCollapseNavClasses(menuItem.title, routeTitle)}>
                     <a href={menuItem.url}>
                       {menuItem.title}
                     </a>
                   </li>
                 )}
               </ul>
-            </div>
+            </Collapse>
           </div>
         </div>
       </header>
