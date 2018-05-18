d84ebd063a34cb99cce88ea88cf41cd479267969
Johannes Katajamäki <jkatajamki@gmail.com>
2018-05-18 21:10:20 +0300

    error boundary

--- client/js/components/home/Home.jsx
+++ client/js/components/home/Home.jsx
@@ -3,23 +3,21 @@ import {
   Jumbotron, Button
 } from 'reactstrap';
 
-const Home = () => {
-  return (
-    <div>
-      <Jumbotron>
-        <div className="container text-center">
-          <h1 className="jumbotron-heading">Lo-fi cliche authentic ennui</h1>
-          <p className="lead text-muted">
-            Schlitz neutra brooklyn sustainable pabst bicycle rights viral everyday carry normcore forage poutine. Tumblr yuccie church-key banh mi, keffiyeh authentic venmo banjo woke flannel butcher selfies live-edge.
-          </p>
-          <div className="buttons-area">
-            <Button color="primary" className="my-2">Snackwave</Button>
-            <Button color="secondary" className="my-2">Fingerstache</Button>
-          </div>
+const Home = () => (
+  <div>
+    <Jumbotron>
+      <div className="container text-center">
+        <h1 className="jumbotron-heading">Lo-fi cliche authentic ennui</h1>
+        <p className="lead text-muted">
+          Schlitz neutra brooklyn sustainable pabst bicycle rights viral everyday carry normcore forage poutine. Tumblr yuccie church-key banh mi, keffiyeh authentic venmo banjo woke flannel butcher selfies live-edge.
+        </p>
+        <div className="buttons-area">
+          <Button color="primary" className="my-2">Snackwave</Button>
+          <Button color="secondary" className="my-2">Fingerstache</Button>
         </div>
-      </Jumbotron>
-    </div>
-  );
-};
+      </div>
+    </Jumbotron>
+  </div>
+);
 
 export default Home;
