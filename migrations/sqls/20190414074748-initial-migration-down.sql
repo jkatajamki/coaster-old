f0ad208509a1e8f1b967256e5934906e7ae4a204
Johannes Katajamäki <jkatajamki@gmail.com>
2019-04-19 10:54:52 +0300

    Initial migrations with db-migrate

--- migrations/sqls/20190414074748-initial-migration-down.sql
+++ migrations/sqls/20190414074748-initial-migration-down.sql
@@ -1,1 +1,11 @@
+drop table if exists user_albums;
+
+drop table if exists albums;
+
+drop table if exists artists;
+
 drop table if exists coaster_users;
+
+drop type album_types;
+
+drop type album_formats;
--- migrations/sqls/20190414074748-initial-migration-up.sql
+++ migrations/sqls/20190414074748-initial-migration-up.sql
@@ -1,5 +1,5 @@
 create table if not exists coaster_users (
-  id bigserial not null primary key,
+  user_id bigserial not null primary key,
   username varchar(255) not null unique,
   created_at timestamp not null,
   email varchar(255) not null unique,
@@ -9,11 +9,56 @@ create table if not exists coaster_users (
 );
 
 create table if not exists artists (
-  id bigserial not null primary key,
+  artist_id bigserial not null primary key,
   artist_name varchar(511) not null,
   artist_slug varchar(255) unique not null,
   genre varchar(255),
   country varchar(255),
+  created_at timestamp not null,
+  updated_at timestamp not null
+);
+
+create type album_types as enum (
+  'full-length',
+  'ep',
+  'demo',
+  'compilation',
+  'video',
+  'live-album',
+  'single',
+  'split',
+  'collab',
+  'box-set'
+);
+
+create table if not exists albums (
+  album_id bigserial not null primary key,
+  artist_id bigserial not null references artists (artist_id),
+  description text,
+  release_year int,
+  release_month int,
+  release_day int,
+  album_type album_types,
+  created_at timestamp not null,
+  updated_at timestamp not null
+);
+
+create type album_formats as enum (
+  'cd',
+  'cassette',
+  '12-inch',
+  '7-inch',
+  'digital',
+  'dvd',
+  'video'
+);
+
+create table if not exists user_albums (
+  user_album_id bigserial not null primary key,
+  album_id bigserial not null references albums (album_id),
+  user_id bigserial not null references coaster_users (user_id),
+  format album_formats,
+  edition_description text,
   created_at timestamp not null,
   updated_at timestamp not null
 );
