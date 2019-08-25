72f767c1366ddfab6cba1753a6fe58e5f8f65f54
Johannes Katajamäki <jkatajamki@gmail.com>
2019-08-25 22:38:44 +0300

    Fix alert closing

--- client/js/alerts/Alerts.jsx
+++ client/js/alerts/Alerts.jsx
@@ -9,6 +9,40 @@ const getAlertBoxClassNames = alertClass => `alert-box alert alert-dismissable $
 
 const getCloseIconClassNames = alertClass => `close-alert-box-icon ${alertClass}`;
 
+const AlertBox = ({ alert, handleClose }) => {
+  const requestClose = () => {
+    const { type, alertClass } = alert;
+    handleClose(type, alertClass);
+  }
+
+  return (
+    <div key={alert.id} className="row">
+      <div className="col-sm-4 offset-sm-4">
+        <div className={getAlertBoxClassNames(alert.alertClass)}>
+          <div className="alert-box-header">
+            <strong className="alert-title">{alert.title}</strong>
+            <button
+              aria-label="close"
+              className="close close-alert-box"
+              type="button"
+              title="Close"
+              onClick={requestClose}
+            >
+              <FontAwesomeIcon
+                className={getCloseIconClassNames(alert.alertClass)}
+                icon={faTimes}
+              />
+            </button>
+          </div>
+          <div className="alert-message">
+            <p>{alert.message}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
 class Alerts extends PureComponent {
   constructor(props) {
     super(props);
@@ -27,35 +61,7 @@ class Alerts extends PureComponent {
 
     return (
       <div id="alertsBlock" className="px-5">
-        { allAlerts.map(alert => (
-          <div key={alert.id} className="row">
-            <div className="col-sm-4 offset-sm-4">
-              <div className={getAlertBoxClassNames(alert.alertClass)}>
-
-                <div className="alert-box-header">
-                  <strong className="alert-title">{alert.title}</strong>
-                  <button
-                    aria-label="close"
-                    className="close close-alert-box"
-                    type="button"
-                    title="Close"
-                    onClick={handleClose}
-                  >
-                    <FontAwesomeIcon
-                      className={getCloseIconClassNames(alert.alertClass)}
-                      icon={faTimes}
-                    />
-                  </button>
-                </div>
-
-                <div className="alert-message">
-                  <p>{alert.message}</p>
-                </div>
-
-              </div>
-            </div>
-          </div>
-        ))}
+        { allAlerts.map(alert => <AlertBox alert={alert} handleClose={handleClose} />)}
       </div>
     );
   }
