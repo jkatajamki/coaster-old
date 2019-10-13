a6401d311aa36e20f9a6937b51c84ddd9b45b4f5
Johannes Katajamäki <jkatajamki@gmail.com>
2019-10-13 14:49:20 +0300

    Load env config

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
-import _ from './load-env';
 import express from 'express';
 import bodyParser from 'body-parser';
+import _ from './load-env'; // eslint-disable-line no-unused-vars
 import serverIsRunningMessage from './utils/server-is-running-message';
 import router from './routing/routes';
 import { authenticationMiddleware } from './authentication/auth-middleware';
